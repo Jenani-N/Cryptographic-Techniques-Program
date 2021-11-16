7e4a8437d956ab160a8e3a94be3c4fef69aee77c
Rui Felipe David <ruifelipe@hotmail.com>
2021-11-16 12:32:07 -0500

    Fix vigenere to generate cipher for lower/upper case
    This implementation break the vigenereCipher into
    char functions (for both encrypt and decrypt) and
    convert to uppercase if that is the case

--- vigenere.cpp
+++ vigenere.cpp
@@ -3,41 +3,89 @@
 #include <cctype>
 #include <string>
 
+#define KEY 'B'
+
 using namespace std;
 
+char encryptVigenere(char);
+char decryptVigenere(char);
+
+/* Encrypt a String using Vigenere Cipher */
 string vigenereCipherEncryption(string message) {
-    string encryption = "";
-    for (int i = 0; i < message.length(); i++) {
-        char charac = message[i];
-        int k = 'c';
-        char formula = (int(charac) + k) % 26;
-        if (!isspace(charac)) { //checking for white space
-            encryption += formula + '!';
-        }
-        else {
-            encryption += "  ";
-        }
-    }
-    return encryption;
+  string encryption = "";
+
+  for (int i = 0; i < message.length(); i++) {
+    encryption += encryptVigenere(message[i]);
+  }
+
+  return encryption;
 }
 
+/* Encrypt a char using Vigenere Cipher */
+char encryptVigenere(char charac) {
+  char encryptedChar = '\0'; // NULL
+
+  /* 
+   * Check if it's space character 
+   */
+  if (charac == ' ') {
+    return charac;
+  }
+
+  /* 
+   * As the Vigenere cypher only supports alphabetic, we have to convert
+   * lowercase to uppercase, so we can get the same result in the
+   * requirement (assuming k = 'B'):
+   *
+   * Original Message: natural foundations for information technology
+   * Encrypted Message: obuvsbm gpvoebujpot gps jogpsnbujpo ufdiopmphz
+   *
+   * A = 65, Z = 90
+   * a = 97, z = 122
+   *
+   * Subtract 32 if character lies between a and z
+   */
+  if ((charac >= 'a') && (charac <= 'z')) {
+    charac -= 32;
+  }
+
+  /* Apply formula if it's a valid input (A-Z) */
+  if ((charac >= 'A') && (charac <= 'Z')) {
+    char formula = (int(charac) + KEY) % 26;
+    encryptedChar = formula + 'A';
+  }
+
+  return encryptedChar;
+}
+
+/* Decrypt a string using Vigenere Cipher */
 string vigenereCipherDecryption(string message) {
-    string decryption = "";
-    for (int i = 0; i < message.length(); i++) {
-        char charac = message[i];
-        int k = 'b';
-        char formula = (int(charac) - k + 26) % 26;
-        if (!isspace(charac)) { //checking for white space
-            if (int(charac) >= '!' && int(charac) <= ('@')) {
-                decryption += formula + 26 +'!' + 63;
-            }
-            else {
-                decryption += formula + '!';
-            }
-        }
-        else {
-            decryption += " ";
-        }
-    }
-    return decryption;
+  string decryption = "";
+
+  for (int i = 0; i < message.length(); i++) {
+    decryption += decryptVigenere(message[i]);
+  }
+
+  return decryption;
+}
+
+/* Decrypt a char using Vigenere Cipher */
+char decryptVigenere(char charac) {
+  char decryptedChar = '\0';
+
+  if (charac == ' ') {
+    return charac;
+  }
+
+  /* Same logic for decryption if it's lowercase */
+  if ((charac >= 'a') && (charac <= 'z')) {
+    charac -= 32;
+  }
+
+  if ((charac >= 'A') && (charac <= 'Z')) {
+    char formula = (int(charac) - KEY + 26) % 26;
+    decryptedChar = formula + 'A';
+  }
+
+  return decryptedChar;
 }
